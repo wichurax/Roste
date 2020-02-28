f83693d77567827a4529a594b5bc2c582ae6127f
wichurax <matsowinskiw@gmail.com>
2020-02-28 17:43:34 +0100

    working angular 9 app - RS-1

--- ClientApp/src/app/shared-components/shared-components.module.ts
+++ ClientApp/src/app/shared-components/shared-components.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CalendarComponent } from "./calendar/calendar.component";
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { MsCalendarHeaderComponent } from './calendar/ms-calendar-header/ms-calendar-header.component';
 import { MsCalendarContentComponent } from './calendar/ms-calendar-content/ms-calendar-content.component';
 import { MsDayComponent } from './calendar/ms-calendar-content/ms-day/ms-day.component';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     declarations: [
