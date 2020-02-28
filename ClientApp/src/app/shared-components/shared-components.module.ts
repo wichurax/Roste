33811f8ab2ce478975c6a382ce7f917c5d2a25f9
wichurax <matsowinskiw@gmail.com>
2020-02-28 17:13:31 +0100

    some fixes and bugs commit - RS-1

--- ClientApp/src/app/shared-components/shared-components.module.ts
+++ ClientApp/src/app/shared-components/shared-components.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from "@angular/common";
 import { CalendarComponent } from "./calendar/calendar.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MsCalendarHeaderComponent } from './calendar/ms-calendar-header/ms-calendar-header.component';
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from '@angular/material/button';
 import { MsCalendarContentComponent } from './calendar/ms-calendar-content/ms-calendar-content.component';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MsDayComponent } from './calendar/ms-calendar-content/ms-day/ms-day.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
     declarations: [
