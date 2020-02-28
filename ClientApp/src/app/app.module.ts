f0d4e4de4eb3588f01254a7b7d3caef280fd4f6a
wichurax <matsowinskiw@gmail.com>
2020-02-28 16:45:30 +0100

    angular material update - RS-1

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { SharedComponentsModule } from "./shared-components/shared-components.module";
