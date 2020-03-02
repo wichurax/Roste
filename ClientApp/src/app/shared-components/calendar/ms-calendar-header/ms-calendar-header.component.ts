05aaaef2f4f5e37e9bb45ec6f93b9bc0a5262192
wichurax <matsowinskiw@gmail.com>
2020-03-02 22:10:18 +0100

    added menu to change month  - RS-1

--- ClientApp/src/app/shared-components/calendar/ms-calendar-header/ms-calendar-header.component.ts
+++ ClientApp/src/app/shared-components/calendar/ms-calendar-header/ms-calendar-header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+const monthShortNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
 @Component({
     selector: 'ms-calendar-header',
     templateUrl: './ms-calendar-header.component.html',
@@ -8,28 +12,45 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MsCalendarHeaderComponent implements OnInit {
 
     @Input() activeYear: number;
-    @Input() month: string;
+    @Input() activeMonth: string;
 
     private _quantityOfYearsInGrid = 20;
+    private _quantityOfMonthsInYear = 12;
     public quantityOfColsInMatMenuGrid = 4;
     public yearsShift = 0;
 
     public years: Array<number> = [];
+    public months: Array<string> = [];
+
+    public setYearMenuViewActive = true;
 
     constructor() { }
 
     public ngOnInit() {
-        this.refreshYears();
+        this.loadInitialDataForMenu();
     }
 
-    public setYear(year: number) {
+    public setYear(year: number, event: any) {
         this.activeYear = year;
         this.refreshYears();
+        event.stopPropagation();
+        this.setYearMenuViewActive = false;
+    }
+
+    public setMonth(month: string) {
+        this.activeMonth = month;
+        setTimeout(() => this.setYearMenuViewActive = true, 300);
+    }
+
+    public loadMonths() {
+        for (let i = 0; i < this._quantityOfMonthsInYear; i++) {
+            this.months[i] = monthShortNames[i];
+        }
     }
 
     private refreshYears() {
         for (var i = 0; i < this._quantityOfYearsInGrid; i++) {
-            this.years[i] = (this.activeYear - this.quantityOfColsInMatMenuGrid - (this._quantityOfYearsInGrid * this.yearsShift) + i);
+            this.years[i] = (this.activeYear - this.quantityOfColsInMatMenuGrid + (this._quantityOfYearsInGrid * this.yearsShift) + i);
         }
     }
 
@@ -43,5 +64,24 @@ export class MsCalendarHeaderComponent implements OnInit {
         event.stopPropagation();
         this.yearsShift++;
         this.refreshYears();
+    }
+
+    public incrementYear(event: any) {
+        this.activeYear++;
+        event.stopPropagation();
+    }
+
+    public decrementYear(event: any) {
+        this.activeYear--;
+        event.stopPropagation();
+    }
+
+    public loadInitialDataForMenu() {
+        setTimeout(() => {
+            this.setYearMenuViewActive = true;
+            this.yearsShift = 0;
+            this.refreshYears();
+            this.loadMonths();
+        }, 300);
     }
 }
